35e99f43a71edc92c8a383f0ad1978c65d812f2e
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-07-27 13:05:14 +0000

    Add admin filter, searchbar and prepopulated fieelds

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,10 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    list_display = ('title', 'slug', 'status','created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
 
 
