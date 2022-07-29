71b66281e7c3ad5f6cc369b623de9bb85a3ed1b7
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-07-29 09:43:10 +0000

    Add comment field

--- blog/models.py
+++ blog/models.py
@@ -19,8 +19,24 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
