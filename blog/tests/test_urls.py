5f75a4c2721b9d1c43cb25243b938f7ca18909af
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-08-11 11:15:26 +0000

    test url, homepage and about page

---
+++ blog/tests/test_urls.py
@@ -0,0 +1,25 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve 
+from blog.views import PostList, AboutView
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, PostList)
+
+    def test_about_url_resolves(self):
+        url = reverse('about')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, AboutView)
+
+    def test_PostDetail_url_resolves(self):
+        url = reverse('post_detail', args=['slug'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, PostDetail)
+
+
+
+
